09636269e15c51a8416c3da46d9422eaafd9f8f3
Adriano-Defilippis <ciprianoadrianodefilippis@gmail.com>
2019-07-16 15:50:51 +0200

    Azione click sull'icona per chiudere il menù nascosto

--- main.js
+++ main.js
@@ -9,6 +9,14 @@ console.log(hamburgerList);
 //Azione click sull'icona dell'amburgher per far apparire il menù nascosto
 hamburgerList.click(function(){
 
-  $(".hamburger-menu").fadeIn(2000)
+  $(".hamburger-menu").fadeIn();
+
+
+});
+
+//Azione click sull'icona per chiudere il menù nascosto
+hamburgerListClose.click(function(){
+
+  $(".hamburger-menu").fadeOut(1000);
 
 });
