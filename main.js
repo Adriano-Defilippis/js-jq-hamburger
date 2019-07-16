1794b95b4f7562c3dce4e94f5dd42c756f0d55b2
Adriano-Defilippis <ciprianoadrianodefilippis@gmail.com>
2019-07-16 17:33:53 +0200

    Migliorie aggiunta classi Jquery, Migliorie Css

--- main.js
+++ main.js
@@ -3,29 +3,32 @@
 //Memorizzo l'elemento azione del click con chiamata Jquery in una variabile
 var hamburgerList = $("#hamburgerButton");
 var hamburgerListClose = $(".close");
-
 console.log(hamburgerList);
 
 //Azione click sull'icona dell'amburgher per far apparire il menù nascosto
 hamburgerList.on({
 
   click: function(){
-
     $(".hamburger-menu").fadeIn();
 
     //Aggiunta Classi per ridimensinare il div nascosto
-    $(".hamburger-menu").css({ 'min-width' : "25vw", "width" : "25%", "min-height" : "50vh", "height" : "50%"});;
-
-    // $(".hamburger-menu").css("width", "25vh");
-
+    $(".hamburger-menu").css({ 'min-width' : "35vw", "width" : "40%", "min-height" : "50vh", "height" : "75%"});
   }
 
-
 });
+
 
 //Azione click sull'icona per chiudere il menù nascosto
 hamburgerListClose.click(function(){
 
-  $(".hamburger-menu").fadeOut(1000);
+  $(".hamburger-menu").fadeOut(500);
 
 });
+
+function resize(){
+  if ($(document).width() >= 1000 ) {
+    $(".hamburger-menu").css({ 'display' : "none"});
+  }
+}
+
+$(".hamburger-menu").resize();
